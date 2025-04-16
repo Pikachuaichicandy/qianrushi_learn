33a3e8502c82362ae36a64951e8c8b7bbfac4e33
pikachuaichicandy <3498307689@qq.com>
2025-04-16 23:16:16 +0800

    Boot_Key

---
+++ esp32s3/Boot_Key/main/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+static QueueHandle_t gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    uint32_t io_num;
+    for (;;) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
+
+void app_main(void)
+{
+    // zero-initialize the config structure.
+    gpio_config_t io_conf = {
+        // disable interrupt
+        io_conf.intr_type = GPIO_INTR_NEGEDGE,
+        // set as output mode
+        io_conf.mode = GPIO_MODE_INPUT,
+        // bit mask of the pins that you want to set,e.g.GPIO18/19
+        io_conf.pin_bit_mask = 1ULL << GPIO_NUM_0,
+        // disable pull-down mode
+        io_conf.pull_down_en = 0,
+        // disable pull-up mode
+        io_conf.pull_up_en = 1,
+    };
+    // configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    // create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    // start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    // install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_NUM_0, gpio_isr_handler, (void *)GPIO_NUM_0);
+}

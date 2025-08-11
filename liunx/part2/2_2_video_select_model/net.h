67d03fb9d47a245aef0872ba67da25453a467f35
pikachuaichicandy <3498307689@qq.com>
2025-08-11 21:44:15 +0800

    头文件

---
+++ liunx/part2/2_2_video_select_model/net.h
@@ -0,0 +1,27 @@
+#ifndef __MAKE_NET_H__
+#define __MAKE_NET_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <sys/errno.h>
+#include <strings.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#include <sys/select.h>
+#include <sys/time.h>
+
+
+
+// 3. 宏定义
+#define SEVR_RESP_STR "Server:"
+#define SERV_PORT   5001
+#define SERV_IP_ADDR     "192.168.226.128"
+#define BACKLOG         5   
+#define QUIT_STR    "quit"
+#endif
+
